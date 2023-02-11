ac3599827ff3aafb08c7cd76bc703cda1ab95357
David Arias <gedafu2005@gmail.com>
2023-02-11 16:14:40 -0500

    add initial config

--- src/main/java/com/myorg/app/App.java
+++ src/main/java/com/myorg/app/App.java
@@ -1,10 +1,10 @@
-package com.my-org.app;
+package com.myorg.app;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
--- src/test/java/com/myorg/app/AppTest.java
+++ src/test/java/com/myorg/app/AppTest.java
@@ -1,13 +1,13 @@
-package com.my-org.app;
+package com.myorg.app;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
     /**
      * Rigorous Test :-)
